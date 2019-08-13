76a3fd6d743c25ec5bc7b9c3591acef9f708b54a
JimGerth <jim.frederic.gerth@gmail.com>
2019-08-13 16:24:08 +0200

    combined temporary MainWidget with MainTool and refactored the latter a bit

--- tools/MainTool.py
+++ tools/MainTool.py
@@ -1,4 +1,6 @@
 from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtGui import QGuiApplication
+from PyQt5.QtCore import QRect
 
 from tools.MainToolUI import MainToolUI
 
@@ -11,27 +13,33 @@ class MainTool(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self._view = MainToolUI(parent=self)
+        self._set_screen_size()
 
         self._image_service = ImageService()
         self._image_service.load_images(Defaults.image_data_file_path)
 
         self._connect_buttons()
 
-        self.setCentralWidget(self._view)
+        self.setCentralWidget(MainToolUI(parent=self))
         self._show_next_image()
 
+    def _set_screen_size(self):
+        screen_geometry = QGuiApplication.primaryScreen().availableGeometry()
+        geometry = QRect(0, 0, screen_geometry.width() * 3 / 4, screen_geometry.height() * 3 / 4)
+        geometry.moveCenter(screen_geometry.center())
+        self.setGeometry(geometry)
+
     def _connect_buttons(self):
-        self._view._single_button.clicked.connect(self._image_classified_single)
-        self._view._skip_button.clicked.connect(self._show_random_image)
-        self._view._multi_button.clicked.connect(self._image_classified_multi)
+        self.centralWidget()._single_button.clicked.connect(self._image_classified_single)
+        self.centralWidget()._skip_button.clicked.connect(self._show_random_image)
+        self.centralWidget()._multi_button.clicked.connect(self._image_classified_multi)
 
     def _show_next_image(self):
         try:
             self._image_service.next_image()
         except IndexError:
             print('no more images to classify')
-        self._view._show_image(self._image_service.current_image)
+        self.centralWidget()._show_image(self._image_service.current_image)
 
     def _image_classified_single(self):
         self._image_service.classify_image('single')
