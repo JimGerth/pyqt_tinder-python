734b74fe0602725e3f24c0eeedb702221744da98
JimGerth <jim.frederic.gerth@gmail.com>
2019-08-13 21:52:17 +0200

    removed horizontal and vertical offsets and replaced it with position of type QPointF wich makes the code way more understandable. also moved rotation calculation into property and tried to get an snap back animation to work, but so far it hasnt worked

--- tools/TinderUI.py
+++ tools/TinderUI.py
@@ -1,6 +1,6 @@
 import numpy as np
-from PyQt5.Qt import QSwipeGesture, QPanGesture, QPinchGesture, QGestureRecognizer
-from PyQt5.QtCore import QEvent, Qt
+from PyQt5.Qt import QSwipeGesture, QPanGesture, QPinchGesture, QGestureRecognizer, QPointF, pyqtProperty
+from PyQt5.QtCore import QEvent, Qt, QPropertyAnimation
 from PyQt5.QtGui import QPainter
 from PyQt5.QtWidgets import QWidget
 
@@ -14,9 +14,7 @@ class TinderUI(QWidget, UI):
 
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.position = 0
-        self.horizontal_offset = 0
-        self.vertical_offset = 0
+        self.position = QPointF(0, 0)
         self.scale_factor = 1
         self.current_step_scale_factor = 1
         self.setMinimumSize(100, 100)
@@ -25,6 +23,17 @@ class TinderUI(QWidget, UI):
 
         id = QGestureRecognizer.registerRecognizer(PanGestureRecognizer())
         self.grabGesture(id)
+
+    @property
+    def rotation(self):
+        pic = [self.position.x(), -self.position.y()]
+        pivot = [0, -500]
+        v1 = np.subtract(pic, pivot)
+        v2 = np.subtract([0, 0], pivot)
+        a = np.arccos(np.divide(np.abs(np.dot(v1, v2)), (np.linalg.norm(v1) * np.linalg.norm(v2))))
+        if self.position.x() < 0:
+            return -a * 10
+        return a * 10
 
     def event(self, event):
         if event.type() == QEvent.Gesture: # gesture event
@@ -42,8 +51,8 @@ class TinderUI(QWidget, UI):
         wh = self.height()
 
         painter.translate(ww / 2, wh / 2)
-        painter.translate(self.horizontal_offset, self.vertical_offset)
-        painter.rotate(self.get_rotation())
+        painter.translate(self.position.x(), self.position.y())
+        painter.rotate(self.rotation)
         painter.scale(self.current_step_scale_factor * self.scale_factor, self.current_step_scale_factor * self.scale_factor)
         painter.translate(-iw / 2, -ih / 2)
         if not self._image:
@@ -55,10 +64,14 @@ class TinderUI(QWidget, UI):
         self.reset()
 
     def reset(self):
+        #self.animation = QPropertyAnimation(self, b'position')
+        #self.animation.setStartValue(self.position)
+        #self.animation.setEndValue(QPointF(0, 0))
+        #self.animation.setDuration(100)
+        #self.animation.start()
+        self.position = QPointF(0, 0)
         self.scale_factor = 1
         self.current_step_scale_factor = 1
-        self.vertical_offset = 0
-        self.horizontal_offset = 0
         self.update()
 
     def gesture_event(self, event):
@@ -72,30 +85,13 @@ class TinderUI(QWidget, UI):
 
     def pan_triggered(self, pan_gesture):
         delta = pan_gesture.delta()
-        self.horizontal_offset += delta.x()
-        self.vertical_offset += delta.y()
+        self.position += delta
         self.update()
         if pan_gesture.state() == Qt.GestureFinished:
             self.reset()
 
-    def pinch_triggered(self, pinch_gesture):
-        print('pinch triggered: {}'.format(pinch_gesture))
-
-    def swipe_triggered(self, swipe_gesture):
-        print('swipe triggered: {}'.format(swipe_gesture))
-
     def resizeEvent(self, event):
         self.update()
-
-    def get_rotation(self):
-        pic = [self.horizontal_offset, -self.vertical_offset]
-        pivot = [0, -500]
-        v1 = np.subtract(pic, pivot)
-        v2 = np.subtract([0, 0], pivot)
-        a = np.arccos(np.divide(np.abs(np.dot(v1, v2)), (np.linalg.norm(v1) * np.linalg.norm(v2))))
-        if self.horizontal_offset < 0:
-            return -a * 10
-        return a * 10
 
     def show_image(self, image, cmap='Spectral_r', interpolation='gaussian'):
         self._image = PlotService().convert_to_image(image, cmap)
