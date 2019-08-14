afeefa66f00bc269574444d348ce2bc1ad9571ec
JimGerth <jim.frederic.gerth@gmail.com>
2019-08-14 13:43:47 +0200

    moved images_to_classify_manually.h5 into data section

--- data/Defaults.py
+++ data/Defaults.py
@@ -1,6 +1,6 @@
 
 class Defaults:
 
-    image_data_file_path = 'images_to_classify_manually.h5'
+    image_data_file_path = 'data/images_to_classify_manually.h5'
     output_path = 'output.csv'
     cmap = 'Spectral_r'
