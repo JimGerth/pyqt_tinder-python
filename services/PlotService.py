66196807940497bdd11801b03027a0d70f33d008
JimGerth <jim.frederic.gerth@gmail.com>
2019-08-16 11:46:35 +0200

    apparently matplotlib cmaps can be applied to arrays as well. this means i dont have to loop over every single pixel to set the colormap. so: colormaps working and images load perfectly fast without any lag.

--- services/PlotService.py
+++ services/PlotService.py
@@ -1,6 +1,4 @@
-from PyQt5.QtGui import QImage
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtGui import QColor
 from matplotlib import colors
 from matplotlib import cm
 import qimage2ndarray as i2a
@@ -11,19 +9,12 @@ from data.Defaults import Defaults
 class PlotService:
 
     def convert_to_image(self, image, cmap=Defaults.cmap):
-
-        # norm = colors.Normalize(vmin=0, vmax=image.data.max())
-        # cmap = cm.get_cmap(cmap)
-        #
-        # for x in range(image.width):
-        #     for y in range(image.height):
-        #         image.data[x][y] = list(cmap(norm(image.data[x][y])))
-
-        q_image = i2a.array2qimage(image.data, normalize=True)
+        norm = colors.Normalize(vmin=image.data.min(), vmax=image.data.max())
+        cmap = cm.get_cmap(cmap)
+        normalized_colored_data = cmap(norm(image.data))
+        q_image = i2a.array2qimage(normalized_colored_data, normalize=True)
         image.q_image = q_image
-        return q_image
 
     def convert_to_pixmap(self, image, cmap=Defaults.cmap):
         q_pixmap = QPixmap.fromImage(self.convert_to_image(image, cmap))
         image.q_pixmap = q_pixmap
-        return q_pixmap
