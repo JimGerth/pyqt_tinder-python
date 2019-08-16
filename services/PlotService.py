cd9bd53d5e90119921bde8e505a2bec5e408b6f2
JimGerth <jim.frederic.gerth@gmail.com>
2019-08-16 11:31:16 +0200

    added qimage2ndarray module to render the images way faster, this drastically improves performance, however colormaps cnat be applied anymore...

--- services/PlotService.py
+++ services/PlotService.py
@@ -3,6 +3,7 @@ from PyQt5.QtGui import QPixmap
 from PyQt5.QtGui import QColor
 from matplotlib import colors
 from matplotlib import cm
+import qimage2ndarray as i2a
 
 from data.Defaults import Defaults
 
@@ -10,20 +11,15 @@ from data.Defaults import Defaults
 class PlotService:
 
     def convert_to_image(self, image, cmap=Defaults.cmap):
-        q_image = QImage(image.width, image.height, QImage.Format_ARGB32)
-        norm = colors.Normalize(vmin=0, vmax=image.data.max())
-        cmap = cm.get_cmap(cmap)
 
-        for x in range(image.width):
-            for y in range(image.height):
-                rgba = cmap(norm(image.data[x][y]))
-                color = QColor()
-                color.setRed(rgba[0] * 255)
-                color.setGreen(rgba[1] * 255)
-                color.setBlue(rgba[2] * 255)
-                color.setAlpha(rgba[3] * 255)
-                q_image.setPixel(x, y, color.rgb())
+        # norm = colors.Normalize(vmin=0, vmax=image.data.max())
+        # cmap = cm.get_cmap(cmap)
+        #
+        # for x in range(image.width):
+        #     for y in range(image.height):
+        #         image.data[x][y] = list(cmap(norm(image.data[x][y])))
 
+        q_image = i2a.array2qimage(image.data, normalize=True)
         image.q_image = q_image
         return q_image
 
