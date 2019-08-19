b82598631368f4462c7eaf1d459e2d59a2c1dac7
JimGerth <jim.frederic.gerth@gmail.com>
2019-08-19 13:01:14 +0200

    saving is now easiy with the shortcuts and a new save as action and a QFileDialog making selection the output path easy

--- tools/MainTool.py
+++ tools/MainTool.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QMainWindow, QMessageBox, QAction, QMenu, QMenuBar
+from PyQt5.QtWidgets import QMainWindow, QMessageBox, QAction, QMenu, QMenuBar, QFileDialog
 from PyQt5.QtGui import QGuiApplication, QKeySequence
 from PyQt5.QtCore import QRect
 
@@ -18,13 +18,18 @@ class MainTool(QMainWindow):
         super().__init__(parent)
         self._application = application
         self._saved = False
+        self._save_path = None
 
         self.setMenuBar(QMenuBar())
         self.save_action = QAction('Save')
         self.save_action.setShortcut(QKeySequence.Save)
         self.save_action.triggered.connect(self._save_results)
+        self.save_as_action = QAction('Save as...')
+        self.save_as_action.setShortcut(QKeySequence.SaveAs)
+        self.save_as_action.triggered.connect(self._save_as_results)
         self.file_menu = QMenu('File')
         self.file_menu.addAction(self.save_action)
+        self.file_menu.addAction(self.save_as_action)
         self.menuBar().addMenu(self.file_menu)
 
         self._ui = TinderUI(parent=self)
@@ -83,6 +88,7 @@ class MainTool(QMainWindow):
         thank_you_screen.setText('Thank you!')
         thank_you_screen.setInformativeText('You classified all 200 images.')
         thank_you_screen.setIcon(QMessageBox.Information)
+        thank_you_screen.setContentsMargins(25, 25, 25, 25)
         thank_you_screen.buttonClicked.connect(self._exit)
         thank_you_screen.exec()
 
@@ -90,5 +96,11 @@ class MainTool(QMainWindow):
         self._save_results()
         self._application.exit()
 
-    def _save_results(self, path=Defaults.output_path):
-        self._image_service.save_results(path)
+    def _save_results(self):
+        if not self._save_path:
+            self._save_path = QFileDialog.getSaveFileName(self, 'save classifications', './output.csv', 'CSV Files (*.csv)')[0]
+        self._image_service.save_results(self._save_path)
+
+    def _save_as_results(self):
+        self._save_path = None
+        self._save_results()
