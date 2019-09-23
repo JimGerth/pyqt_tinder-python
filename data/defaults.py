94058dbbdd1c6b7278c22a810ca59910e2b949a7
JimGerth <jim.frederic.gerth@gmail.com>
2019-09-23 15:24:21 +0200

    added env variable to defaults to be able to open up application with command line

--- data/defaults.py
+++ data/defaults.py
@@ -4,15 +4,16 @@ from PyQt5.QtCore import Qt
 
 class Defaults:
 
-    image_data_file_path = 'data/images_to_classify_manually.h5'
-    output_path = 'output.csv'
+    env = '/Users/jim/PycharmProjects/pyqt_tinder-python/'
+    image_data_file_path = env + 'data/images_to_classify_manually.h5'
+    output_path = env + 'output.csv'
     cmap = 'Spectral_r'
     pen = QPen(Qt.NoPen)
     single_color = QColor(22, 153, 0, 130) #169900
     skip_color = QColor(255, 194, 35, 130) #FFC223
     multi_color = QColor(236, 0, 28, 130)  #EC001C
     sensibility = 1.5
-    title_card_file_path = 'data/title_card_2.png'
-    check_icon_path = 'data/single.png'
-    clock_icon_path = 'data/circle_clock.png'
-    cross_icon_path = 'data/multi2.png'
+    title_card_file_path = env + 'data/title_card_2.png'
+    check_icon_path = env + 'data/single.png'
+    clock_icon_path = env + 'data/circle_clock.png'
+    cross_icon_path = env + 'data/multi2.png'
